a66cfa708d93955626418b0b0868df8794778e1a
70705 <totos@riseup.net>
2024-02-16 20:56:44 -0300

    changed kernel, changed pipewire to lowlatency and added nix-gaming

--- home.nix
+++ home.nix
@@ -95,7 +95,14 @@
     commandLineArgs = [ "--enable-blink-features=MiddleClickAutoscroll"
                         "--enable-features=VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization,WebUIDarkMode"
                         "--force-dark-mode"
-                        "--start-maximized" ];
+                        "--start-maximized"
+			"--enable-zero-copy"
+			"--ignore-gpu-blocklist"
+			"--enable-oop-rasterization"
+			"--enable-raw-draw"
+			"--enable-gpu-rasterization"
+			"--use-vulkan"
+			"--disable-gpu-sandbox"];
   };
 
 
