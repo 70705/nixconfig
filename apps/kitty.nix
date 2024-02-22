9ba813639a2b9e3ef457e9e0968a4bab4f506cbb
70705 <totos@riseup.net>
2024-02-22 07:42:54 -0300

    removed kitty.conf

--- apps/kitty.nix
+++ apps/kitty.nix
@@ -3,15 +3,23 @@
 {
    programs.kitty = {
      enable = true;
-     extraConfig = builtins.readFile ./kitty.conf;
+     settings = {
+       remember_window_size = "yes";
+       initial_window_width = 640;
+       initial_window_height = 400;
+       mouse_hide_wait = 0;
+       enable_layouts = "grid";
+    };
+
      shellIntegration.enableZshIntegration = true;
      package = pkgs-unstable.kitty;
      theme = "Dark Pastel";
      keybindings = { "ctrl+shift+left" = "previous_window"; "ctrl+shift+right" = "next_window"; "ctrl+q" = "close_window"; };
+
      font = {
-      name = "Hack";
-      package = pkgs.hack-font;
-      size = 14;
+       name = "Hack";
+       package = pkgs.hack-font;
+       size = 14;
     };
   };
 }     
