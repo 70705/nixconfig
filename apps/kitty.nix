24873802da0b86d7ea8166cde1fbd11f2c9cfe76
70705 <totos@riseup.net>
2024-02-22 07:07:51 -0300

    kitty shit

--- apps/kitty.nix
+++ apps/kitty.nix
@@ -3,8 +3,15 @@
 {
    programs.kitty = {
      enable = true;
+     extraConfig = builtins.readFile ./kitty.conf;
      shellIntegration.enableZshIntegration = true;
      package = pkgs-unstable.kitty;
      theme = "Dark Pastel";
+     keybindings = { "ctrl+shift+left" = "previous_window"; "ctrl+shift+right" = "next_window"; "ctrl+q" = "close_window"; };
+     font = {
+      name = "Hack";
+      package = pkgs.hack-font;
+      size = 14;
+    };
   };
 }     
