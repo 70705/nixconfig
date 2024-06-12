4eb078f76589ccd8fcbe8ff3562d808bb2a45e66
70705 <totos@riseup.net>
2024-06-12 08:20:57 -0300

    I did some sillies

--- pkgs/utils/git.nix
+++ pkgs/utils/git.nix
@@ -7,9 +7,6 @@
       enable = true;
       userName = "70705";
       userEmail = "totos@riseup.net";
-      extraConfig = {
-        credential.helper = "oauth";
-      };
     };
 
     git-credential-oauth.enable = true;
