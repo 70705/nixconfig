fcf3608845ccf6a1ffb101dfadd77755b474cfcb
70705 <totos@riseup.net>
2024-06-18 13:51:43 -0300

    Removed syntax error and added pipewire config

--- pkgs/media/mpd.nix
+++ pkgs/media/mpd.nix
@@ -8,8 +8,19 @@
   services = {
     mpd = {
       enable = true;
-      packages = pkgs-unstable.mpd; 
+      package = pkgs-unstable.mpd; 
       musicDirectory = /media/gamedisk2/Musicas;
+      dataDir = /etc/nixos/pkgs/media/mpdData;
+      extraConfig = ''
+        audio_output {
+          type    "pipewire"
+          name    "MPD PipeWire Output"
+        }
+      '';
+    };
+
+    mpdris2 = {
+      enable = true;
     };
   };
 }
