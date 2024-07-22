e5d96bd95fd1f99fd2d621aa84d2661f7f95b02e
70705 <totos@riseup.net>
2024-07-22 02:48:35 -0300

    Some Hyprland QOL

--- pkgs/hypr/hyprland.nix
+++ pkgs/hypr/hyprland.nix
@@ -8,6 +8,8 @@
   wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    systemd.variables = ["--all"];
+
     plugins = [];
     extraConfig = builtins.readFile ./hyprland.conf;
   };
