841f4b3843c018a84bf789e0d20441576e3befc6
70705 <totos@riseup.net>
2024-07-21 22:22:34 -0300

    Updated yazi desktop entry

--- pkgs/utils/yazi.nix
+++ pkgs/utils/yazi.nix
@@ -14,7 +14,7 @@
     yazi = {
       name = "Yazi";
       comment = "Blazing fast terminal file manager written in Rust, based on async I/O";
-      exec = "wezterm start yazi %f";
+      exec = "kitty yazi %f";
       mimeType = [ "inode/directory" ];
       categories = [ "System" "FileTools" "FileManager" "Utility" "Core" ];
       terminal = false;
