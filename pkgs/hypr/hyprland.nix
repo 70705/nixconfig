a558ce96f85b43b05ed2791aaec5e4d1a04b2850
70705 <totos@riseup.net>
2024-07-24 06:53:18 -0300

    Initial hyprland ricing (Not in a Nix-way for now.)

--- pkgs/hypr/hyprland.nix
+++ pkgs/hypr/hyprland.nix
@@ -17,7 +17,9 @@
   services.hyprpaper = {
     enable = true;
     settings = {
+      preload = [ "/etc/nixos/pkgs/hypr/wallpaper.png" ];
       wallpaper = [",/etc/nixos/pkgs/hypr/wallpaper.png"];
+
     };
   };
 
