4a71a9d482852223b7787dca42e1d418c8b36632
70705 <totos@riseup.net>
2024-07-29 09:26:26 -0300

    Moved some packages, added audio thumbnailer

---
+++ pkgs/utils/audiothumbs.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{
+  home.file = {
+  ".local/share/thumbnailers/audiothumbs.thumbnailer".text = ''
+    [Thumbnailer Entry]
+    TryExec=ffmpeg
+    Exec=ffmpeg -y -i %i %o -fs %s
+    MimeType=audio/mpeg;audio/flac;audio/wavpack;audio/webm;audio/mp4;audio/aac;audio/x-matroska;audio/x-opus+ogg    '';
+  };
+}
