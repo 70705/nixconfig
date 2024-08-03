d9507484177a193725a6442a7c0eae5db2b0c321
70705 <totos@riseup.net>
2024-08-03 14:05:32 -0300

    Removed qute

--- pkgs/browsers/default.nix
+++ pkgs/browsers/default.nix
@@ -5,16 +5,13 @@
 
 {
   imports = [
-    ./qute.nix
     ./vivaldi.nix
-    ./firefox/firefox.nix
   ];
 
   home.packages = with pkgs; [
     ungoogled-chromium
-    python312Packages.adblock # used for qute's adblock
+#    python312Packages.adblock # used for qute's adblock
     widevine-cdm
-
   ];
 
 }
