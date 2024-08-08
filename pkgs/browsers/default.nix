d4af70dd15a45191cceb3ccfb6b48f6f8ceb4811
70705 <totos@riseup.net>
2024-08-08 12:35:35 -0300

    Disabled vivaldi

--- pkgs/browsers/default.nix
+++ pkgs/browsers/default.nix
@@ -5,12 +5,11 @@
 
 {
   imports = [
-    ./vivaldi.nix
+    ./firefox/firefox.nix
   ];
 
   home.packages = with pkgs; [
     ungoogled-chromium
-#    python312Packages.adblock # used for qute's adblock
     widevine-cdm
   ];
 
