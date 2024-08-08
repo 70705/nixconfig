484394af230170a4ffff8cbe036c1d1634491f6b
70705 <totos@riseup.net>
2024-08-08 12:44:33 -0300

    Added some XDG stuff

--- home.nix
+++ home.nix
@@ -22,18 +22,28 @@
       pkgs.xdg-desktop-portal-gtk
       inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
     ];
+    configPackages = [
+      pkgs.xdg-desktop-portal-gtk
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
+    ];
   };
 
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      "x-scheme-handler/https"= [ "vivaldi-stable.desktop" "org.qutebrowser.qutebrowser.desktop"];
-      "x-scheme-handler/http" = [ "vivaldi-stable.desktop" "org.qutebrowser.qutebrowser.desktop"];
-      "text/html" = [ "vivaldi-stable.desktop" "org.qutebrowser.qutebrowser.desktop" ];
-      "x-scheme-handler/about" = [ "vivaldi-stable.desktop" "org.qutebrowser.qutebrowser.desktop"];
+      "x-scheme-handler/https"= "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "application/pdf" = "firefox.desktop";
       "inode/directory" = ["yazi.desktop"];
-      "x-scheme-handler/trash" = ["pcmanfm.desktop"];
-      "application/zip" = ["xarchiver.desktop"];
+      "x-scheme-handler/trash" = "pcmanfm.desktop";
+      "image/webp" = "org.nomacs.ImageLounge.desktop";
+      "image/png" = "org.nomacs.ImageLounge.desktop";
+      "image/jpeg" = "org.nomacs.ImageLounge.desktop";
+      "image/jpg" = "org.nomacs.ImageLounge.desktop";
+      "image/gif" = "org.nomacs.ImageLounge.desktop";
+#      "application/zip" = ["xarchiver.desktop"];
     };
   };
 
