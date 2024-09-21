e6b137e87cd8cdc8f057981d92f7f0c6c393c79a
70705 <totos@riseup.net>
2024-09-21 14:49:53 -0300

    Starship module

---
+++ modules/hm/shells/starship.nix
@@ -0,0 +1,80 @@
+{ ... }:
+
+{
+  programs.starship = {
+    enable = true;
+    enableTransience = true;
+    enableZshIntegration = true;
+    settings = {
+      add_newline = true;
+      format = [
+        "$time"
+        "\${custom.nix}"
+        "$line_break"
+        "$cmd_duration"
+        "$character"
+      ];
+      right_format = [
+        "$directory"
+        "$git_branch"
+        "$git_state"
+        "$git_status"
+      ];
+
+      time = {
+        disabled = false;
+        format = "[''\''[$time''\'']]($bold white) ";
+        time_format = "%T";
+      };
+
+      hostname = {
+        ssh_only = false;
+        format = "[$hostname](#acacac) on ";
+        disabled = true;
+      };
+      directory = {
+        truncation_length = 2;
+        style = "bold white";
+      };
+
+      custom.nix = {
+        command = "nix-shell-info | awk '{print substr($0, 8, length($0)-11)}'";
+        when = "test 0 -ne $(nix-shell-info | wc -w)";
+        format = "''\\''[[$output](bold white)''\\'']";
+        os = "linux";
+      };
+
+      character = {
+        success_symbol = "[%](bold bright-white)";
+        error_symbol = "[!](red)";
+        vimcmd_symbol = "[%](green)";
+      };
+
+      git_branch = {
+        format = "➜ [$branch]($style) ";
+        style = "bold white";
+      };
+
+      git_status = {
+        format = "[[($all_status)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+      };
+
+      git_state = {
+        format = ''\([$state( $progress_current/$progress_total)]($style)\) '';
+        style = "bold white";
+      };
+
+      cmd_duration = {
+        format = "[$duration]($style) ";
+        style = "bold white";
+      };
+
+      python = {
+        format = "[$virtualenv]($style) ";
+        style = "bold white";
+      };
+
+    };
+  };
+}
