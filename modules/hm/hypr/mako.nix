1868a730cb95e3c1ade6cf0cd6b68d71835eeed4
70705 <totos@riseup.net>
2024-09-20 21:35:34 -0300

    Configured Hyprland and MPV in a nix way

--- modules/hm/hypr/mako.nix
+++ modules/hm/hypr/mako.nix
@@ -8,7 +8,7 @@
   services.mako = {
     enable = lib.mkIf config.hmModules.hypr.enable true; 
     ignoreTimeout = true;
-    defaultTimeout = 4500;
+    defaultTimeout = 7500;
     borderRadius = 3;
     borderSize = 2;
   };
