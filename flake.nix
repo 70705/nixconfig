91bc04d52b04e88a5b7a44b3c5e292aaea5d3486
70705 <totos@riseup.net>
2024-09-20 21:36:48 -0300

    Flake.nix formatting

--- flake.nix
+++ flake.nix
@@ -1,63 +1,75 @@
 {
   description = "victor's Flake";
 
-  outputs = { self, nixpkgs, nur, home-manager, ... }@inputs: let
-    definedVars = {
-      username = "victor"; 
-      hostname = "wired";
-      timezone = "America/Recife";
-      locale = "pt_BR.UTF-8";
-      
-      extraLocale = [ 
-        "en_US.UTF-8/UTF-8"
-        "pt_BR.UTF-8/UTF-8"
-        "C.UTF-8/UTF-8"
-        "ja_JP.UTF-8/UTF-8"
-      ];
-      system = "x86_64-linux";
-    };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nur,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      definedVars = {
+        username = "victor";
+        hostname = "wired";
+        timezone = "America/Recife";
+        locale = "pt_BR.UTF-8";
 
-  in {
-    nixosConfigurations = {
-      ${definedVars.hostname} = nixpkgs.lib.nixosSystem { 
-        system = definedVars.system;
-
-        specialArgs = {
-          inherit inputs;
-          inherit definedVars;
-        };
-
-        modules = [
-          ./hosts/${definedVars.hostname}/configuration.nix
-          home-manager.nixosModules.home-manager
-          inputs.stylix.nixosModules.stylix
-          inputs.nix-index-database.nixosModules.nix-index
-          inputs.nix-gaming.nixosModules.platformOptimizations
-
-          { programs.nix-index-database.comma.enable = true; }
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.${definedVars.username} = import ./hosts/${definedVars.hostname}/home.nix;
-              extraSpecialArgs = {
-                inherit inputs;
-                inherit definedVars;
-              };
-            };
-          }
+        extraLocale = [
+          "en_US.UTF-8/UTF-8"
+          "pt_BR.UTF-8/UTF-8"
+          "C.UTF-8/UTF-8"
+          "ja_JP.UTF-8/UTF-8"
         ];
+        system = "x86_64-linux";
+      };
+
+    in
+    {
+      nixosConfigurations = {
+        ${definedVars.hostname} = nixpkgs.lib.nixosSystem {
+          system = definedVars.system;
+
+          specialArgs = {
+            inherit inputs;
+            inherit definedVars;
+          };
+
+          modules = [
+            ./hosts/${definedVars.hostname}/configuration.nix
+            home-manager.nixosModules.home-manager
+            inputs.stylix.nixosModules.stylix
+            inputs.nix-index-database.nixosModules.nix-index
+            inputs.nix-gaming.nixosModules.platformOptimizations
+
+            { programs.nix-index-database.comma.enable = true; }
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.${definedVars.username} = import ./hosts/${definedVars.hostname}/home.nix;
+                extraSpecialArgs = {
+                  inherit inputs;
+                  inherit definedVars;
+                };
+              };
+            }
+          ];
+        };
       };
     };
-  };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixvim-config.url = "github:70705/nixvim-config";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     nur.url = "github:nix-community/NUR";
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nix-alien = {
       url = "github:thiagokokada/nix-alien";
@@ -88,5 +100,5 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  }; 
+  };
 }
