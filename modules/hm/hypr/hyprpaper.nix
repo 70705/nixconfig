1e0910439753d73b615e1b6b923b3556763a5b59
70705 <totos@riseup.net>
2024-09-20 21:35:34 -0300

    Configured Hyprland and MPV in a nix way

--- modules/hm/hypr/hyprpaper.nix
+++ modules/hm/hypr/hyprpaper.nix
@@ -1,15 +1,16 @@
-{ 
-  lib, 
+{
+  lib,
   config,
-  ... 
+  ...
 }:
 
 {
   services.hyprpaper = {
-    enable = lib.mkIf config.hmModules.hypr.enable true; 
+    stylix.targets.hyprpaper.enable = false;
+    enable = lib.mkIf config.hmModules.hypr.enable true;
     settings = {
       preload = [ "/etc/nixos/modules/hm/hypr/wallpaper.png" ];
-      wallpaper = [",/etc/nixos/modules/hm/hypr/wallpaper.png"];
+      wallpaper = [ ",/etc/nixos/modules/hm/hypr/wallpaper.png" ];
     };
   };
 }
