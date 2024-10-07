66420391816f15663a0d9f1b545ffba399bcf6f7
70705 <totos@riseup.net>
2024-10-06 21:27:07 -0300

    Changed Spicetify config

--- modules/hm/media/spicetify.nix
+++ modules/hm/media/spicetify.nix
@@ -1,35 +1,34 @@
-{ 
+{
   pkgs,
   inputs,
-  lib, 
+  lib,
   config,
-  ... 
+  ...
 }:
 
 let
   cfg = config.hmModules.media.spotify;
 in
-  {
-    imports = [ inputs.spicetify-nix.homeManagerModules.default ];
-    options.hmModules.media.spotify = {
-      enable = lib.mkEnableOption "spotify";
-    };
+{
+  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
+  options.hmModules.media.spotify = {
+    enable = lib.mkEnableOption "spotify";
+  };
 
-    config = lib.mkIf cfg.enable {
-      programs.spicetify =
-        let
-          spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-        in
-        {
-          enable = true;
-          enabledExtensions = with spicePkgs.extensions; [
-            fullAppDisplay
-            shuffle # shuffle+ (special characters are sanitized out of ext names)
-            adblock
-            hidePodcasts
-            history
-          ];
-          theme = spicePkgs.themes.text;
-        };
+  config = lib.mkIf cfg.enable {
+    programs.spicetify =
+      let
+        spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+      in
+      {
+        enable = true;
+        enabledExtensions = with spicePkgs.extensions; [
+          fullAppDisplay
+          shuffle # shuffle+ (special characters are sanitized out of ext names)
+          adblock
+          hidePodcasts
+          history
+        ];
       };
-    }
+  };
+}
