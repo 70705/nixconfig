78063496d21dfce6ce0012eb3fee71b70fe2c5c0
70705 <totos@riseup.net>
2024-10-06 21:29:17 -0300

    Some emacs changes

--- modules/hm/utils/emacs.nix
+++ modules/hm/utils/emacs.nix
@@ -14,5 +14,46 @@
     nixfmt-rfc-style
   ];
 
+  xdg.desktopEntries = {
+    "emacsclient" = {
+      name = "Emacs (Client)";
+      exec = ''sh -c "if [ -n \\"\\$*\\" ]; then exec /nix/store/67znrwn1wjcd78pf4hhpbrl2rsh14nha-emacs-pgtk-29.4/bin/emacsclient --alternate-editor= --reuse-frame \\"\\$@\\"; else exec emacsclient --alternate-editor= --create-frame; fi" sh %F'';
+      noDisplay = true;
+    };
+
+    "emacs" = {
+      name = "Emacs";
+      exec = "emacsclient -r %F";
+      genericName = "Text Editor";
+      comment = "Edit text";
+      mimeType = [
+        "text/english"
+        "text/plain"
+        "text/x-makefile"
+        "text/x-c++hdr"
+        "text/x-c++src"
+        "text/x-chdr"
+        "text/x-csrc"
+        "text/x-java"
+        "text/x-moc"
+        "text/x-pascal"
+        "text/x-tcl"
+        "text/x-tex"
+        "application/x-shellscript"
+        "text/x-c"
+        "text/x-c++"
+      ];
+      icon = "emacs";
+      type = "Application";
+      terminal = false;
+      categories = [
+        "Development"
+        "TextEditor"
+      ];
+      startupNotify = true;
+
+    };
+  };
+
   stylix.targets.emacs.enable = false;
 }
