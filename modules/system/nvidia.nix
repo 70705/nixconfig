bb6767f60c9106172c2866106c667cc9035ae702
70705 <totos@riseup.net>
2024-10-12 00:00:03 -0300

    Conditionally enabling nvidia-settings if using X11

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -1,22 +1,28 @@
 {
   config,
-  ... 
+  ...
 }:
 
 {
 
-  hardware.graphics = { 
+  hardware.graphics = {
     enable = true;
     enable32Bit = true;
-   };
+  };
 
-   services.xserver.videoDrivers = ["nvidia"];
-  
-   hardware.nvidia = {
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
     open = false;
-    nvidiaSettings = true;
+    nvidiaSettings =
+      if config.modules.system.graphical.hyprland.enable then
+        false
+      else if config.modules.system.graphical.i3.enable then
+        true
+      else
+        false;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
-   };
+  };
 }
